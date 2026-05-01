c0124f18151e25b4715a5bf4b149a42e35c50fb3
Rostislav <rost.tm@gmail.com>
2026-05-01 18:09:28 +0300

    feat: add mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,36 @@ document.addEventListener('DOMContentLoaded', () => {
     header.addEventListener('click', () => {
       const isOpen = item.classList.contains('active');
 
-      // Закрываем все открытые секции
       accordionItems.forEach(i => {
         i.classList.remove('active');
         i.querySelector('.accordion-body').style.maxHeight = null;
       });
 
-      // Если кликнули по закрытой — открываем её
       if (!isOpen) {
         item.classList.add('active');
         const body = item.querySelector('.accordion-body');
         body.style.maxHeight = body.scrollHeight + "px";
+      }
+    });
+  });
+
+  const burgerBtn = document.getElementById('burgerBtn');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const body = document.body;
+  const menuLinks = document.querySelectorAll('.nav-list-mobile a');
+
+  function toggleMenu() {
+    burgerBtn.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+  }
+
+  burgerBtn.addEventListener('click', toggleMenu);
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      if (mobileMenu.classList.contains('active')) {
+        toggleMenu();
       }
     });
   });
